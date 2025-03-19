cc4d9cb890dbc59f6e7756cc3a70a968e4de6e6f
Kamran Allah Yar <kamranallahyar@outlook.com>
2025-03-20 00:45:35 +0500

    fix for axios

--- quasar.config.js
+++ quasar.config.js
@@ -46,8 +46,8 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-build
     build: {
       env: {
-        API: ctx.dev ? "http://127.0.0.1:8000" : ''
-        
+        API: ctx.dev ? "http://127.0.0.1:8000" : '/'
+
         //
       },
       vueRouterMode: "hash", // available values: 'hash', 'history'
